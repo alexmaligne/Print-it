cf3d84bf2d6872870184a6b6fd132ac88cb41973
Alexandra Maligne <alexandra.maligne@gmail.com>
2023-04-25 19:01:45 +0200

    projet finalisé

--- assets/script.js
+++ assets/script.js
@@ -17,27 +17,19 @@ const slides = [
 	}
 ]
 
+let diaporama = 0
 let arrowLeft = document.querySelector("#banner .arrow_left");
 let arrowRight = document.querySelector("#banner .arrow_right");
+let imageSlider = document.querySelector(".banner-img");
+let texteSlider = document.querySelector("#banner p");
 
-	arrowLeft.addEventListener("click", function () {
 
-		alert("fleche gauche")
-		console.log("fleche gauche")
-});
-
-	arrowRight.addEventListener("click", function () {
-
-		alert("fleche droite")
-		console.log("fleche droite")
-});
-
-let diaporama = 0
-let codeHtml =""
+function bulletPoints() {
+	let codeHtml =""
 let dots = document.querySelector(".dots");
 
 for(let images = 0; images<slides.length; images++){ 
-	
+
 	if(images == diaporama){codeHtml += "<div class='dot dot_selected'></div>"}
 	else{
 		codeHtml += "<div class='dot'></div>"
@@ -45,3 +37,29 @@ for(let images = 0; images<slides.length; images++){
 }
 
 dots.innerHTML = codeHtml
+}
+
+bulletPoints()
+
+	arrowLeft.addEventListener("click", function () {
+		diaporama = diaporama - 1;
+
+		if(diaporama < 0) {
+			diaporama = 3
+		}
+		
+		imageSlider.src = "./assets/images/slideshow/" + slides[diaporama].image;
+		texteSlider.innerHTML = slides[diaporama].tagLine;
+		bulletPoints();
+});
+
+	arrowRight.addEventListener("click", function () {
+		diaporama = diaporama + 1;
+
+	if(diaporama > 3) {
+		diaporama = 0
+	}
+		imageSlider.src = "./assets/images/slideshow/" + slides[diaporama].image;
+		texteSlider.innerHTML = slides[diaporama].tagLine;
+		bulletPoints();
+});
